04319d3ee38cf1192a2210a6cef0ff13bee062ec
damianfe <damianrocio12@gmail.com>
2024-05-22 11:21:15 -0300

    fix: show Sidebar only after user successfully logs in

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -13,12 +13,15 @@ const Header: React.FC = () => {
   return (
     <AppBar position="fixed" color="primary">
       <Toolbar>
-        <Box display="flex" justifyContent="space-between" width="100%">
+        <Box display="flex" justifyContent="space-between" alignItems="center" width="100%">
           {/* Logo */}
-          <Typography variant="h6" component="div">
-            <img src="/path/to/your/logo.png" alt="Logo" style={{ height: 40 }} />
-          </Typography>
-          {/* Logeo Icon */}
+          <Box display="flex" alignItems="center">
+            <img src="/path/to/your/logo.png" alt="Logo" style={{ height: 40, marginRight: 10 }} />
+            <Typography variant="h6" component="div">
+              Time Keeper
+            </Typography>
+          </Box>
+          {/* Login Icon */}
           <IconButton edge="end" color="inherit" aria-label="login" onClick={handleIconClick}>
             <AccountCircleIcon />
           </IconButton>
