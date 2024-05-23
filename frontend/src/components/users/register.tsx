2ab1d1db8bdd1d7bf027257bf950f6a90a0d7a30
Kevin <kevinpernia29@gmail.com>
2024-05-23 11:43:44 -0500

    [UPDATE] setting user role

--- frontend/src/components/users/register.tsx
+++ frontend/src/components/users/register.tsx
@@ -1,9 +1,27 @@
-import React, { useState, FormEvent } from 'react';
-import { Box, Button, Divider, IconButton, TextField, Typography, InputAdornment, MenuItem, Select, FormControl, InputLabel } from '@mui/material';
-import { FaGithub, FaGoogle, FaLinkedin } from 'react-icons/fa';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import { validateName, validateEmail, validatePassword, validatePhone, validateBirthDate } from './validations';
+import React, { useState, FormEvent } from "react";
+import {
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  TextField,
+  Typography,
+  InputAdornment,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+} from "@mui/material";
+import { FaGithub, FaGoogle, FaLinkedin } from "react-icons/fa";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import {
+  validateName,
+  validateEmail,
+  validatePassword,
+  validatePhone,
+  validateBirthDate,
+} from "./validations";
 
 interface Errors {
   firstName: string;
@@ -11,84 +29,116 @@ interface Errors {
   email: string;
   password: string;
   confirmPassword: string;
-  role: string;
+  role: number;
+  isRole: string;
   birthDate: string;
   phone: string;
 }
 
 const Register: React.FC = () => {
-  const [firstName, setFirstName] = useState<string>('');
-  const [lastName, setLastName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [confirmPassword, setConfirmPassword] = useState<string>('');
-  const [role, setRole] = useState<string>('');
-  const [birthDate, setBirthDate] = useState<string>('');
-  const [phone, setPhone] = useState<string>('');
-  const [errors, setErrors] = useState<Errors>({ firstName: '', lastName: '', email: '', password: '', confirmPassword: '', role: '', birthDate: '', phone: '' });
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [role, setRole] = useState<number>(0);
+  const [isRole, setIsRole] = useState("")
+  const [birthDate, setBirthDate] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [errors, setErrors] = useState<Errors>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    role: 0,
+    isRole: "",
+    birthDate: "",
+    phone: "",
+  });
   const [showPassword, setShowPassword] = useState<boolean>(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState<boolean>(false);
-
+  const [showConfirmPassword, setShowConfirmPassword] =
+    useState<boolean>(false);
+  const [valid, setValid] = useState<boolean>(true);
   const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-    let valid = true;
-    const newErrors: Errors = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '', role: '', birthDate: '', phone: '' };
+    const newErrors: Errors = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      role: 0,
+      isRole: "",
+      birthDate: "",
+      phone: "",
+    };
 
     if (!validateName(firstName)) {
-      newErrors.firstName = 'Please enter a valid first name.';
-      valid = false;
+      newErrors.firstName = "Please enter a valid first name.";
+      setValid(false);
     }
 
     if (!validateName(lastName)) {
-      newErrors.lastName = 'Please enter a valid last name.';
-      valid = false;
+      newErrors.lastName = "Please enter a valid last name.";
+      setValid(false);
     }
 
     if (!validateEmail(email)) {
-      newErrors.email = 'Please enter a valid email address.';
-      valid = false;
+      newErrors.email = "Please enter a valid email address.";
+      setValid(false);
     }
 
     if (!validatePassword(password)) {
-      newErrors.password = 'Password must be 8-30 characters long and include a number, a symbol, an uppercase and a lowercase letter.';
-      valid = false;
+      newErrors.password =
+        "Password must be 8-30 characters long and include a number, a symbol, an uppercase and a lowercase letter.";
+      setValid(false);
     }
 
     if (password !== confirmPassword) {
-      newErrors.confirmPassword = 'Passwords do not match.';
-      valid = false;
+      newErrors.confirmPassword = "Passwords do not match.";
+      setValid(false);
     }
 
     if (!role) {
-      newErrors.role = 'Please select your role.';
-      valid = false;
+      newErrors.isRole = "Please select your role.";
+      setValid(false);
     }
 
     if (!birthDate || !validateBirthDate(birthDate)) {
-      newErrors.birthDate = 'Please enter a valid birth date.';
-      valid = false;
+      newErrors.birthDate = "Please enter a valid birth date.";
+      setValid(false);
     }
 
-    if (phone && !validatePhone(phone)) {
-      newErrors.phone = 'Please enter a valid phone number.';
-      valid = false;
+    if (!phone && !validatePhone(phone)) {
+      newErrors.phone = "Please enter a valid phone number.";
+      setValid(false);
     }
-
     setErrors(newErrors);
 
     if (valid) {
+      isRole === "Freelancer" ? setRole(1) : setRole(2);
       // Submit the form or perform other actions
-      console.log('Form submitted:', { firstName, lastName, email, password, role, birthDate, phone });
+      console.log("Form submitted:", {
+        firstName,
+        lastName,
+        email,
+        password,
+        role,
+        birthDate,
+        phone,
+      });
     }
 
-    setFirstName('');
-    setLastName('');
-    setEmail('');
-    setPassword('');
-    setConfirmPassword('');
-    setRole('');
-    setBirthDate('');
-    setPhone('');
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+    setRole(0);
+    setIsRole("");
+    setBirthDate("");
+    setPhone("");
   };
 
   const handleClickShowPassword = () => {
@@ -109,33 +159,17 @@ const Register: React.FC = () => {
       bgcolor="#f5f5f5"
     >
       <Box textAlign="center" mb={3}>
-        <Typography
-          variant="h5"
-          component="h2"
-          mb={1}
-        >
+        <Typography variant="h5" component="h2" mb={1}>
           Get started with TimeTracker
         </Typography>
-        <Typography
-          variant="body1"
-        >
-          Create a free account to start tracking time and supercharge your productivity.
+        <Typography variant="body1">
+          Create a free account to start tracking time and supercharge your
+          productivity.
         </Typography>
       </Box>
 
-      <Box
-        p={4}
-        bgcolor="white"
-        borderRadius={2}
-        boxShadow={3}
-        width={300}
-      >
-        <Typography
-          variant="h5"
-          component="h2"
-          textAlign="center"
-          mb={3}
-        >
+      <Box p={4} bgcolor="white" borderRadius={2} boxShadow={3} width={300}>
+        <Typography variant="h5" component="h2" textAlign="center" mb={3}>
           Sign Up
         </Typography>
         <form onSubmit={handleSubmit}>
@@ -180,7 +214,7 @@ const Register: React.FC = () => {
             <TextField
               fullWidth
               label="Password"
-              type={showPassword ? 'text' : 'password'}
+              type={showPassword ? "text" : "password"}
               variant="outlined"
               size="small"
               value={password}
@@ -198,7 +232,7 @@ const Register: React.FC = () => {
                       {showPassword ? <Visibility /> : <VisibilityOff />}
                     </IconButton>
                   </InputAdornment>
-                )
+                ),
               }}
             />
           </Box>
@@ -206,7 +240,7 @@ const Register: React.FC = () => {
             <TextField
               fullWidth
               label="Confirm Password"
-              type={showConfirmPassword ? 'text' : 'password'}
+              type={showConfirmPassword ? "text" : "password"}
               variant="outlined"
               size="small"
               value={confirmPassword}
@@ -224,24 +258,33 @@ const Register: React.FC = () => {
                       {showConfirmPassword ? <Visibility /> : <VisibilityOff />}
                     </IconButton>
                   </InputAdornment>
-                )
+                ),
               }}
             />
           </Box>
           <Box mb={2}>
-            <FormControl fullWidth variant="outlined" size="small" error={!!errors.role}>
+            <FormControl
+              fullWidth
+              variant="outlined"
+              size="small"
+              error={!!errors.role}
+            >
               <InputLabel id="role-label">Role</InputLabel>
               <Select
                 labelId="role-label"
                 id="role"
-                value={role}
-                onChange={(e) => setRole(e.target.value as string)}
+                value={isRole}
+                onChange={(e) => setIsRole(e.target.value as string)}
                 label="Role"
               >
                 <MenuItem value="Freelancer">Freelancer</MenuItem>
                 <MenuItem value="Client">Client</MenuItem>
               </Select>
-              {errors.role && <Typography variant="body2" color="error">{errors.role}</Typography>}
+              {errors.isRole && (
+                <Typography variant="body2" color="error">
+                  {errors.isRole}
+                </Typography>
+              )}
             </FormControl>
           </Box>
           <Box mb={2}>
@@ -256,7 +299,7 @@ const Register: React.FC = () => {
               error={!!errors.birthDate}
               helperText={errors.birthDate}
               InputLabelProps={{
-                shrink: true
+                shrink: true,
               }}
             />
           </Box>
@@ -272,36 +315,20 @@ const Register: React.FC = () => {
               helperText={errors.phone}
             />
           </Box>
-          <Button
-            fullWidth
-            type="submit"
-            variant="contained"
-            color="primary"
-          >
+          <Button fullWidth type="submit" variant="contained" color="primary">
             Sign Up
           </Button>
         </form>
-        <Divider
-          sx={{ my: 3 }}
-        >
-          <Typography
-            variant="body2"
-            color="textSecondary"
-          >
+        <Divider sx={{ my: 3 }}>
+          <Typography variant="body2" color="textSecondary">
             Or Continue With
           </Typography>
         </Divider>
         <Box display="flex" justifyContent="center" gap={2}>
-          <IconButton
-            aria-label="google"
-            color="error"
-          >
+          <IconButton aria-label="google" color="error">
             <FaGoogle />
           </IconButton>
-          <IconButton
-            aria-label="linkedin"
-            color="primary"
-          >
+          <IconButton aria-label="linkedin" color="primary">
             <FaLinkedin />
           </IconButton>
           <IconButton aria-label="github" color="inherit">
@@ -314,11 +341,14 @@ const Register: React.FC = () => {
           textAlign="center"
           mt={3}
         >
-          Already have an account? <a href="/login" style={{ color: '#1976d2' }}>Login</a>
+          Already have an account?{" "}
+          <a href="/login" style={{ color: "#1976d2" }}>
+            Login
+          </a>
         </Typography>
       </Box>
     </Box>
-  )
+  );
 };
 
 export default Register;
