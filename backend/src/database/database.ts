5192dd405733c2ace98d23b4672dda0ba1acd1ea
FrancoEspinozaV <franco.espinoza.val@gmail.com>
2024-05-26 21:59:05 -0400

    documentation and fixed bugs

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize";
-import { DATABASE_URL } from "../utils/config";
+import { Sequelize } from 'sequelize'
+import { DATABASE_URL } from '../utils/config'
 
-export const sequelize = new Sequelize(DATABASE_URL as string);
+export const sequelize = new Sequelize(DATABASE_URL as string)
--- backend/src/middlewares/apiKey.middleware.ts
+++ backend/src/middlewares/apiKey.middleware.ts
@@ -1,24 +1,24 @@
-import { NextFunction, Request, Response } from "express";
-import { Key } from "../utils/config";
+import { NextFunction, Request, Response } from 'express'
+import { Key } from '../utils/config'
 
-const authorizedHosts = ["localhost:3000"];
+const authorizedHosts = ['localhost:3000']
 
-export const isApiKey = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const isApiKey = (req: Request, res: Response, next: NextFunction) => {
   try {
-    const apiKey = req.headers.apikey;
+    const apiKey = req.headers.apikey
     if (!apiKey || Key !== apiKey) {
-      return res.status(404).json({ message: "El api key no es valido." });
+      return res.status(404).json({ message: 'El api key no es valido.' })
     }
-    const host = req.headers.host;
+    const host = req.headers.host
     if (!host || !authorizedHosts.includes(host)) {
-      return res.status(404).json({ message: "La dirección de la petición no esta autorizada." });
+      return res
+        .status(404)
+        .json({ message: 'La dirección de la petición no esta autorizada.' })
     }
-    next();
+    next()
   } catch (error) {
-    return res.status(500).json({ message: "El api key tiene un error interno del Servidor." });
+    return res
+      .status(500)
+      .json({ message: 'El api key tiene un error interno del Servidor.' })
   }
-};
+}
