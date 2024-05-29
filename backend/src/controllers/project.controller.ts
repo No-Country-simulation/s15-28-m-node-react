b50a7880221fc828b2609dc90fb2592c43002a19
FrancoEspinozaV <franco.espinoza.val@gmail.com>
2024-05-29 10:00:05 -0400

    add new CRUD

--- backend/src/controllers/project.controller.ts
+++ backend/src/controllers/project.controller.ts
@@ -1,104 +1,84 @@
-import { Request, Response } from 'express';
-import { Project } from '../models/project.model';
-import { Status } from '../models/status.model';
-import { User } from '../models/users.model';
+import { Request, Response } from 'express'
+import { Project } from '../models/project.model'
+import { optionalFieldBody, requeriedFieldsBody } from '../utils/helpers'
 
-export class ProjectController {
-    static async createProject(req: Request, res: Response) {
-        const {
-            start_date,
-            end_date,
-            name,
-            hourly_rate,
-            description,
-            client_id,
-            isCompleted,
-            isActive,
-            status_id,
-            user_id,
-            team_id,
-        } = req.body;
+export async function createProject(req: Request, res: Response) {
+  try {
+    const { body } = req
 
-        try {
-            // Verificar si el cliente existe
-            const client = await User.findByPk(client_id);
-            if (!client) {
-                return res.status(404).json({ message: "Client not found" });
-            }
+    const [error, message] = requeriedFieldsBody({
+      body: body,
+      model: Project,
+      excludedFields: ['uuid'],
+    })
 
-            // Verificar si el status existe
-            const status = await Status.findByPk(status_id);
-            if (!status) {
-                return res.status(404).json({ message: "Status not found" });
-            }
+    if (error !== 200) return res.status(error).json(message)
 
-            // Crear el proyecto
-            const project = await Project.create({
-                start_date,
-                end_date,
-                name,
-                hourly_rate,
-                description,
-                client_id,
-                isCompleted,
-                isActive,
-                status_id,
-                user_id,
-                team_id,
-            });
+    body.uuid = crypto.randomUUID()
+    const project = await Project.create(body)
+    return res.status(201).json(project)
+  } catch (error) {
+    console.error('Error creating project:', error)
+    return res.status(500).json({ message: 'Internal server error' })
+  }
+}
 
-            return res.status(201).json(project);
-        } catch (error) {
-            console.error("Error creating project:", error);
-            return res.status(500).json({ message: "Internal server error" });
-        }
+export async function getProjects(_req: Request, res: Response) {
+  try {
+    const projects = await Project.findAll()
+    return res.status(200).json(projects)
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' })
+  }
+}
+
+export async function getProjectById(req: Request, res: Response) {
+  try {
+    const { id } = req.params
+    const project = await Project.findByPk(id)
+    if (!project) {
+      return res.status(404).json({ message: 'Project not found' })
+    }
+    return res.status(200).json(project)
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' })
+  }
+}
+
+export async function updateProject(req: Request, res: Response) {
+  try {
+    const { id } = req.params
+    const { body } = req
+    const [error, message] = optionalFieldBody({
+      body: body,
+      model: Project,
+      excludedFields: ['uuid'],
+    })
+
+    if (error !== 200) return res.status(error).json(message)
+
+    await Project.update(body, { where: { id } })
+
+    const updatedProject = await Project.findByPk(id)
+
+    if (!updatedProject) {
+      return res.status(404).json({ message: 'Project not found' })
     }
 
-    static async getProjects(_req: Request, res: Response) {
-        try {
-            const projects = await Project.findAll();
-            return res.status(200).json(projects);
-        } catch (error) {
-            return res.status(500).json({ message: "Internal server error" });
-        }
-    }
+    return res.status(200).json(updatedProject)
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' })
+  }
+}
 
-    static async getProjectById(req: Request, res: Response) {
-        try {
-            const project = await Project.findByPk(req.params.id);
-            if (!project) {
-                return res.status(404).json({ message: "Project not found" });
-            }
-            return res.status(200).json(project);
-        } catch (error) {
-            return res.status(500).json({ message: "Internal server error" });
-        }
+export async function deleteProject(req: Request, res: Response) {
+  try {
+    const deleted = await Project.destroy({ where: { id: req.params.id } })
+    if (deleted) {
+      return res.status(204).json()
     }
-
-    static async updateProject(req: Request, res: Response) {
-        try {
-            const [updated] = await Project.update(req.body, {
-                where: { id: req.params.id },
-            });
-            if (updated) {
-                const updatedProject = await Project.findByPk(req.params.id);
-                return res.status(200).json(updatedProject);
-            }
-            return res.status(404).json({ message: "Project not found" });
-        } catch (error) {
-            return res.status(500).json({ message: "Internal server error" });
-        }
-    }
-
-    static async deleteProject(req: Request, res: Response) {
-        try {
-            const deleted = await Project.destroy({ where: { id: req.params.id } });
-            if (deleted) {
-                return res.status(204).json();
-            }
-            return res.status(404).json({ message: "Project not found" });
-        } catch (error) {
-            return res.status(500).json({ message: "Internal server error" });
-        }
-    }
+    return res.status(404).json({ message: 'Project not found' })
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' })
+  }
 }
